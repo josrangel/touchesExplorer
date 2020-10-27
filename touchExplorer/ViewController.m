f1ffec79f33d398a191ecbc9c0990c7d2b53a4b0
josue rangel <josrangel@hotmail.com>
2020-10-27 16:20:21 -0600

    Se agrega funcionalidad

--- touchExplorer/ViewController.m
+++ touchExplorer/ViewController.m
@@ -8,6 +8,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tapsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *touchesLabel;
+
 
 @end
 
@@ -18,5 +22,35 @@
     // Do any additional setup after loading the view.
 }
 
+-(void) updateLabelsFromTouches:(NSSet *) touches{
+    NSUInteger numTaps = [[touches anyObject] tapCount];
+    NSString *tapsMessage = [[NSString alloc]initWithFormat:@"%lu tapsdetected", (unsigned long)numTaps];
+    self.tapsLabel.text = tapsMessage;
+    NSUInteger numTouches = [touches count];
+    NSString *touchMsg = [[NSString alloc] initWithFormat:@"%lu touches detected", (unsigned long)numTouches];
+    self.touchesLabel.text = touchMsg;
+    
+}
+//pragma mark - Touch Event Methods
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent*)event{self.messageLabel.text = @"Touches Began";
+    [self updateLabelsFromTouches:touches];
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent*)event{
+    self.messageLabel.text = @"Touches Cancelled";
+    [self updateLabelsFromTouches:touches];
+    
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent*)event{
+    self.messageLabel.text = @"Touches Ended.";
+    [self updateLabelsFromTouches:touches];
+    
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent*)event{
+    self.messageLabel.text = @"Drag Detected";
+    [self updateLabelsFromTouches:touches];
+}
 
 @end
